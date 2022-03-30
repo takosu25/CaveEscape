60120818025acb7bc7d4abb51c3cfd90217c4092
KotaYoshioka <supakouta@gmail.com>
2022-03-30 20:08:18 +0900

    樽でマグマを潰せるようにしました。

--- CaveEscape/src/main/java/ce/CEBarrel.java
+++ CaveEscape/src/main/java/ce/CEBarrel.java
@@ -1,5 +1,6 @@
 package ce;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.FallingBlock;
@@ -8,6 +9,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class CEBarrel {
 	Plugin plugin;
@@ -20,7 +22,8 @@ public class CEBarrel {
 		this.player = player;
 		this.ceg = ceg;
 		barrel = (FallingBlock)player.getWorld().spawnFallingBlock(player.getEyeLocation(), Material.BARREL, (byte)0);
-		barrel.setVelocity(player.getLocation().getDirection().normalize().multiply(2));
+		barrel.setVelocity(player.getLocation().getDirection().normalize().multiply(1.5));
+		hit();
 	}
 	void hit() {
 		new BukkitRunnable() {
@@ -30,16 +33,29 @@ public class CEBarrel {
 					return;
 				}
 				if(barrel.getLocation().getBlock().getType() == Material.LAVA) {
-					//TODO マグマをつぶす処理
+					barrel.remove();
+					lavaToObsidian(barrel.getLocation().clone());
+					return;
 				}
-				for(Entity ent:barrel.getNearbyEntities(1, 1, 1)) {
+				for(Entity ent:barrel.getNearbyEntities(1.4, 1.4, 1.4)) {
 					if(ent == ceg.getMaster()) {
 						((Player)ent).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 50, 2));
 						barrel.remove();
 					}
 				}
 			}
-		}.runTaskTimer(plugin, 0, 4);
+		}.runTaskTimer(plugin, 0, 2);
 	}
 
+	void lavaToObsidian(Location l) {
+		l.getBlock().setType(Material.OBSIDIAN);
+		Vector[] vs = {new Vector(1,0,0),new Vector(-1,0,0),new Vector(0,1,0), new Vector(0,-1,0),new Vector(0,0,1),new Vector(0,0,-1)};
+		for(Vector v:vs) {
+			Location cl = l.clone();
+			cl.add(v);
+			if(cl.getBlock().getType() == Material.LAVA) {
+				lavaToObsidian(cl.clone());
+			}
+		}
+	}
 }
